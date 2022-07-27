af115e984a41240fd98569a5eff5041b553308db
LinkedList <https://github.com/Juancamilo21/LinkedList.git>
2022-07-27 15:59:46 -0500

    finished

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductController } from './product/product.controller';
 import { ProductEntity } from './product/product.entity';
 import { ProductModule } from './product/product.module';
-import { ProductServise } from './product/product.service';
 
 @Module({
   imports: [
@@ -15,11 +11,11 @@ import { ProductServise } from './product/product.service';
       port: 3306,
       username: 'root',
       password: '',
-      database: 'controlproductos',
-      entities: [ ProductEntity ],
+      database: '',
+      entities: [ProductEntity],
       synchronize: true,
     }),
-    ProductModule
-  ]
+    ProductModule,
+  ],
 })
 export class AppModule {}
